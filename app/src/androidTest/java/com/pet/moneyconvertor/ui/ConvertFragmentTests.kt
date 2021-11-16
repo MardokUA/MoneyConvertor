6d07e189ec3f8d7d2a3f3936f5ec3c75249a8c88
lolenid <lolenid@gmail.com>
2021-11-16 18:46:38 +0700

    Finished adding tests.

--- app/src/androidTest/java/com/pet/moneyconvertor/ui/ConvertFragmentTests.kt
+++ app/src/androidTest/java/com/pet/moneyconvertor/ui/ConvertFragmentTests.kt
@@ -6,44 +6,50 @@ import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.matcher.RootMatchers.withDecorView
 import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.google.android.material.internal.ContextUtils.getActivity
 import com.pet.moneyconvertor.R
+import com.pet.moneyconvertor.adapters.CurrencyAdapter
 import org.hamcrest.Matchers.`is`
 import org.hamcrest.Matchers.not
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import timber.log.Timber
 
 
 @RunWith(AndroidJUnit4::class)
 class ConvertFragmentTests {
-    @Before
-    fun setup() {
-       ActivityScenario.launch(MainActivity::class.java)
+    @get:Rule
+    val activityRule = ActivityScenarioRule(MainActivity::class.java)
 
-    }
+
     @Test
     fun leftCurrencyButtonIsDisplayed() {
-        onView(withId(R.id.buttonLeft)).check(matches(isDisplayed()))
-        onView(withId(R.id.buttonLeft)).check(matches(isClickable()))
-        onView(withId(R.id.buttonLeft)).check(matches(isEnabled()))
+        val button = withId(R.id.buttonLeft)
+        onView(button).check(matches(isDisplayed()))
+        onView(button).check(matches(isClickable()))
+        onView(button).check(matches(isEnabled()))
     }
 
     @Test
     fun rightCurrencyButtonIsDisplayed() {
-        val button = R.id.buttonRight
-        onView(withId(button)).check(matches(isDisplayed()))
-        onView(withId(button)).check(matches(isClickable()))
-        onView(withId(button)).check(matches(isEnabled()))
+        val button = withId(R.id.buttonRight)
+        onView(button).check(matches(isDisplayed()))
+        onView(button).check(matches(isClickable()))
+        onView(button).check(matches(isEnabled()))
     }
 
     @Test
     fun textEditValueIsNotFocusable() {
         onView(withId(R.id.editTextValue)).check(matches(isNotFocusable()))
     }
+
     @Test
     fun textEditValueClickWhileIsNotFocusable() {
         onView(withId(R.id.editTextValue)).perform(click())
@@ -61,8 +67,52 @@ class ConvertFragmentTests {
             )
         )
     }
-    @Test
-    fun selectCurrency() {
 
+    @Test
+    fun selectLeftCurrency() {
+        onView(withId(R.id.buttonLeft)).perform(click())
+        onView(withId(R.id.currencyList))
+            .perform(
+                RecyclerViewActions
+                    .actionOnItemAtPosition<CurrencyAdapter.CurrencyViewHolder>(
+                        0, click()
+                    )
+            )
+        onView(withId(R.id.textViewLeft)).check(matches(withText("AUD")))
+    }
+    @Test
+    fun selectRightCurrency() {
+        onView(withId(R.id.buttonRight)).perform(click())
+        onView(withId(R.id.currencyList))
+            .perform(
+                RecyclerViewActions
+                    .actionOnItemAtPosition<CurrencyAdapter.CurrencyViewHolder>(
+                        1, click()
+                    )
+            )
+        onView(withId(R.id.textViewRight)).check(matches(withText("AZN")))
+    }
+
+    @Test
+    fun editTextIsFocusableAfterSelectCurrencies() {
+        onView(withId(R.id.buttonLeft)).perform(click())
+        onView(withId(R.id.currencyList))
+            .perform(
+                RecyclerViewActions
+                    .actionOnItemAtPosition<CurrencyAdapter.CurrencyViewHolder>(
+                        0, click()
+                    )
+            )
+
+        onView(withId(R.id.buttonRight)).perform(click())
+        onView(withId(R.id.currencyList))
+            .perform(
+                RecyclerViewActions
+                    .actionOnItemAtPosition<CurrencyAdapter.CurrencyViewHolder>(
+                        1, click()
+                    )
+            )
+
+        onView(withId(R.id.editTextValue)).check(matches(isFocusable()))
     }
 }
